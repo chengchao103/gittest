4cfe53cac9dbfefe8af0cf85005831631c2e07ce
chengchao103 <chengchao103@163.com>
2016-05-26 17:19:55 +0800

    git test

---
+++ src/main/java/com/gome/util/Utils.java
@@ -0,0 +1,12 @@
+package com.gome.util;
+
+/**   
+ * @ClassName:  Utils   
+ * @Description:   
+ * @author: chengchao
+ * @date:   2016年5月26日 下午5:19:07   
+ * @Copyright(c) gome inc Gome Co.,LTD   
+ */
+public class Utils {
+
+}
