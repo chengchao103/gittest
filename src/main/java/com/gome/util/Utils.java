53c11eaa44d917ef0a090ce8f6370afbe4860a6c
chengchao103 <chengchao103@163.com>
2016-05-26 17:37:35 +0800

    fsdfs

--- src/main/java/com/gome/util/Utils.java
+++ src/main/java/com/gome/util/Utils.java
@@ -3,7 +3,7 @@ package com.gome.util;
 /**   
  * @ClassName:  Utils   
  * @Description:   
- * @author: chengchao
+ * @author: chengchao     dasds
  * @date:   2016年5月26日 下午5:19:07   
  * @Copyright(c) gome inc Gome Co.,LTD   
  */ 
