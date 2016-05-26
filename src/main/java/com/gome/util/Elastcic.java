706cb1cfcaa8340c6ecca8e3cd456478b9296e96
chengchao103 <chengchao103@163.com>
2016-05-26 17:28:16 +0800

    dd

---
+++ src/main/java/com/gome/util/Elastcic.java
@@ -0,0 +1,12 @@
+package com.gome.util;
+
+/**   
+ * @ClassName:  Elastcic   
+ * @Description:   
+ * @author: chengchao
+ * @date:   2016年5月26日 下午5:27:28   
+ * @Copyright(c) gome inc Gome Co.,LTD   
+ */
+public class Elastcic {
+
+}
